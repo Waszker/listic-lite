696189b41239279a9109ec138f453ad9192c3e0c
Piotr Waszkiewicz <waszka23@gmail.com>
2025-04-20 18:55:40 +0200

    Add rate limiting to ChatOpenAI with InMemoryRateLimiter configuration

--- ai_agent/config.py
+++ ai_agent/config.py
@@ -1,5 +1,11 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.rate_limiters import InMemoryRateLimiter
 import settings
 
 SYSTEM_PROMPT_PREFIX = "You are a helpful assistant specializing in recipe analysis."
-llm = ChatOpenAI(temperature=0.2, openai_api_key=settings.env_settings.openai_api_key, model_name="gpt-4o")
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=0.1,  # <-- Super slow! We can only make a request once every 10 seconds!!
+    check_every_n_seconds=0.1,  # Wake up every 100 ms to check whether allowed to make a request,
+    max_bucket_size=10,  # Controls the maximum burst size.
+)
+llm = ChatOpenAI(temperature=0.2, openai_api_key=settings.env_settings.openai_api_key, model_name="gpt-4o", rate_limiter=rate_limiter)
